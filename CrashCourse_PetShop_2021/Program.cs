9e5549b132c02d13828967840c242a71670a5c1e
soma199 <70133057+Laszlo199@users.noreply.github.com>
2021-08-27 15:34:31 +0200

    asd

---
+++ CrashCourse_PetShop_2021/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CrashCourse_PetShop_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Malcka");
+        }
+    }
+}
